7439f6ad825ca34c3b819d3952221313616bf6ff
dsubbu1311 <dsubbu1311@gmail.com>
2022-01-24 00:34:05 +0530

    First Commit--2

--- src/com/dsubbu1311/Main.java
+++ src/com/dsubbu1311/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("First Code in Githubs");
         System.out.println("Confict Code in Githubs");
-	//2nd commit    
+	  //2nd commit also added above 1
+        //new line commit
     }
 }
